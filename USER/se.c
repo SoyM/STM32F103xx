1d6eb36a4b3e04ce1b42ed22da0225be88026906
SoyM <huangxinjiang1996@qq.com>
2017-07-12 10:23:05 +0800

    串口

--- USER/se.c
+++ USER/se.c
@@ -1,6 +1,6 @@
 #include "sys.h" 	
 #include "delay.h"	
-#include "led.h"
+#include "usart.h"
 
 //LED初始化
 void LED_Init(void)
@@ -32,11 +32,30 @@ void KEY_Init(void)
 int main(void)
 {				  
 	Stm32_Clock_Init(9);//系统时钟设置
+	uart_init(72,115200);
 	delay_init(72);	  	//延时初始化
 	LED_Init();
 	KEY_Init;
 	while(1)
 	{
+		if (USART_RX_STA & 0x8000)
+		{
+			len = USART_RX_STA & 0x3FFF;//得到此次接收到的数据长度
+			printf("\r\n您发送的消息为:\r\n\r\n");
+			for (t = 0; t<len; t++)
+			{
+				USART1->DR = USART_RX_BUF[t];
+				while ((USART1->SR & 0X40) == 0);//等待发送结束
+			}
+			printf("\r\n\r\n");//插入换行
+			USART_RX_STA = 0;
+		}
+		else
+		{
+			times++;
+			if (times % 200 == 0)printf("请输入数据,以回车键结束\r\n");
+			delay_ms(10);
+		}
 	}	 
 }
 
