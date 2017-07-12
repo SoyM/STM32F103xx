df2730cfe0567a86da8f55b614233d5eaa30f417
SoyM <huangxinjiang1996@qq.com>
2017-07-12 10:12:48 +0800

    按键初始化

--- USER/se.c
+++ USER/se.c
@@ -2,15 +2,11 @@
 #include "delay.h"	
 #include "led.h"
 
-#define LED0 PBout(5)
-#define LED1 PEout(5)
-
-//初始化与LED连接的硬件接口
+//LED初始化
 void LED_Init(void)
 {
 	RCC->APB2ENR |= 1 << 3;    //使能PORTB时钟	   	 
 	RCC->APB2ENR |= 1 << 6;    //使能PORTE时钟	
-
 	GPIOB->CRL &= 0XFF0FFFFF;
 	GPIOB->CRL |= 0X00300000;//PB.5 推挽输出   	 
 	GPIOB->ODR |= 1 << 5;      //PB.5 输出高
@@ -20,19 +16,27 @@ void LED_Init(void)
 	GPIOE->ODR |= 1 << 5;      //PE.5输出高 
 }
 
+//按键初始化
+void KEY_Init(void)
+{
+	RCC->APB2ENR |= 1 << 2;     //使能PORTA时钟
+	RCC->APB2ENR |= 1 << 6;     //使能PORTE时钟
+	GPIOA->CRL &= 0XFFFFFFF0;	//PA0设置成输入，默认下拉	  
+	GPIOA->CRL |= 0X00000008;
+
+	GPIOE->CRL &= 0XFFF000FF;	//PE2~4设置成输入 
+	GPIOE->CRL |= 0X00088800;
+	GPIOE->ODR |= 7 << 2;	   	//PE2~4上拉
+}
+
 int main(void)
 {				  
 	Stm32_Clock_Init(9);//系统时钟设置
 	delay_init(72);	  	//延时初始化
-	LED_Init();		  	
+	LED_Init();
+	KEY_Init;
 	while(1)
 	{
-		LED0=0;
-		LED1=1;
-		delay_ms(300);
-		LED0=1;
-		LED1=0;
-		delay_ms(300);
 	}	 
 }
 
